9b2d3cf9998dc1d4a4da32ee52ac47438f1ceee7
StasPonomaryov <ponomaryov.stas@gmail.com>
2024-04-18 16:06:06 +0300

    feat: output customers count in footer from state

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,13 +1,16 @@
 <template>
   <footer class="site-footer">
     <div class="copyright">&copy; Stas Ponomaryov, 2024</div>
-    <div class="stats">👨: 0; 📝: 0;</div>
+    <div class="stats">👨: {{ customersCount }}; 📝: {{ orders }};</div>
     <div class="build">1.00</div>
   </footer>
 </template>
 
 <script setup>
-
+import { useUserStore } from '#imports';
+const store = useUserStore();
+const { customersCount } = storeToRefs(store);
+const orders = ref(0);
 </script>
 
 <style scoped></style>
