5f6eb3e1581c650d1c82d48e40c1439c509dedc1
StasPonomaryov <ponomaryov.stas@gmail.com>
2024-04-18 16:01:13 +0300

    feat(store): added some store properties and methods;

--- stores/user.ts
+++ stores/user.ts
@@ -1,11 +1,26 @@
 import { defineStore } from 'pinia';
 
+interface ICustomersStore {
+  value: Customers[]
+}
+
 export const useUserStore = defineStore("user", () => {
   const user = ref(null);
+  const customers = reactive<ICustomersStore>({ value: [] });
+  const customersCount = ref(0);
   const isLoggedIn = computed(() => user.value !== null);
+
   function setUser(session: any) {
     user.value = session;
   }
 
-  return { user, isLoggedIn, setUser };
+  function setCustomers(data: Customers[]) {
+    customers.value = data;
+  }
+
+  function setCustomersCount(count: number) {
+    customersCount.value = count;
+  }
+
+  return { user, customers, customersCount, isLoggedIn, setUser, setCustomers, setCustomersCount };
 });
