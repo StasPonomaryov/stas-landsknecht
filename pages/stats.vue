faf96467d380cf6dbb5577b64f97863cd32704c8
StasPonomaryov <ponomaryov.stas@gmail.com>
2024-04-18 16:02:55 +0300

    feat(api): add 'get' endpoint for getting customers and count

--- pages/stats.vue
+++ pages/stats.vue
@@ -1,10 +1,25 @@
 <template>
   <section class="grid cards stats">
     <h2>Statistics</h2>
+    Customers: {{ customersCount }}
   </section>
 </template>
 
 <script setup>
+import { useUserStore } from '#imports';
+const store = useUserStore();
+const { customersCount, isLoggedIn, user } = storeToRefs(store);
+
+if (isLoggedIn.value) {
+  try {
+    const { data: count } = await useFetch(`/api/customers/get/?q=count`);
+    if (count) {
+      store.setCustomersCount(count);
+    }
+  } catch (e) {
+    throw e;
+  }
+}
 useHead({
   title: 'Stats - Landsknecht'
 });
