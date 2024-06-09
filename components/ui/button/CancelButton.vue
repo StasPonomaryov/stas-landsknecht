735c9f77a9bedcbdbcd42d4374926ae0305f541d
StasPonomaryov <ponomaryov.stas@gmail.com>
2024-06-09 10:49:08 +0300

    feat: add CancelButton and UpdateButton components

---
+++ components/ui/button/CancelButton.vue
@@ -0,0 +1,18 @@
+<template>
+  <button class="btn btn-cancel">
+    ⛔Cancel
+  </button>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.btn-cancel {
+  background-color: rgb(var(--error-color));
+  border: 0;
+  color: rgb(var(--valid-text-color));
+  margin-right: 0.4rem;
+}
+</style>
---
+++ components/ui/button/UpdateButton.vue
@@ -0,0 +1,18 @@
+<template>
+  <button class="btn btn-update">
+    ✅Update
+  </button>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.btn-update {
+  background-color: var(--primary-color);
+  border-color: transparent;
+  color: #fff;
+  margin-right: 0.5rem;
+}
+</style>
